f06cf750bd41513faa5efc5e11719159c9e0f5aa
Marko Krstic <mailme@markokrstic.com>
2024-05-04 23:20:44 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,3 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-// import './App.css';
-
-// // Import Icon component and all icons from '@wordpress/icons'
-// import { Icon } from '@wordpress/icons';
-// import * as icons from '@wordpress/icons';
-
-// function App() {
-//   return (
-//     <div className="icon-gallery">
-//       {Object.entries(icons).map(([iconName, iconComponent]) => {
-//         // Filter to render only icon components (ignoring the Icon container component)
-//         if (iconName !== 'Icon') {
-//           return (
-            
-//               <Icon icon={iconComponent} />
-            
-//           );
-//         }
-//         return null;
-//       })}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 // Import Icon component and all icons from '@wordpress/icons'
